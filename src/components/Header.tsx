78a960e39b7cd08616f728d7e8ef21946a44b27e
hakan.basturknewinn.com <forxynet@gmail.com>
2024-08-21 21:29:16 +0300

    updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="flex items-center justify-between px-4 border-b h-14">
       <div className="flex items-center space-x-2 text-sm">
         <Link to="/" className="text-lg font-bold">
-          NPM Registry
+          NPM
         </Link>
       </div>
       <div className="w-full max-w-xl ml-4">
